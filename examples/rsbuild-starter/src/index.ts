fdb507c9a4b18d48e5f5142580a3c197c448cf20
pengzhanbo <volodymyr@foxmail.com>
2024-08-03 03:18:07 +0800

    chore: update examples

--- examples/rsbuild-starter/src/index.ts
+++ examples/rsbuild-starter/src/index.ts
@@ -113,21 +113,21 @@ async function bootstrap() {
   await delay()
 }
 
-function webSocketMock() {
-  const ws = new WebSocket('ws://localhost:5173/socket.io')
-  ws.addEventListener(
-    'open',
-    () => {
-      // eslint-disable-next-line no-console
-      console.log('isOpen')
-    },
-    { once: true },
-  )
-  setTimeout(() => {
-    if (ws.readyState === ws.OPEN)
-      ws.send(JSON.stringify({ type: 'message', payload: { a: 1 } }))
-  }, 3000)
-}
+// function webSocketMock() {
+//   const ws = new WebSocket('ws://localhost:5173/socket.io')
+//   ws.addEventListener(
+//     'open',
+//     () => {
+//       // eslint-disable-next-line no-console
+//       console.log('isOpen')
+//     },
+//     { once: true },
+//   )
+//   setTimeout(() => {
+//     if (ws.readyState === ws.OPEN)
+//       ws.send(JSON.stringify({ type: 'message', payload: { a: 1 } }))
+//   }, 3000)
+// }
 
 bootstrap()
-webSocketMock()
+// webSocketMock()
