babb61210487b2c25a5b798d51c1e05aff409e1b
pengzhanbo <volodymyr@foxmail.com>
2024-08-02 18:01:01 +0800

    chore: bump deps

--- examples/rsbuild-starter/rsbuild.config.ts
+++ examples/rsbuild-starter/rsbuild.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginMockServer } from 'rspack-plugin-mock/rsbuild'
 
 export default defineConfig({
+  html: {
+    template: './index.html',
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': {
+        target: 'http://localhost:8080',
+        onError(err, req, res) {
+          console.error('[proxy:error]', err?.stack || err)
+          res.statusCode = 500
+          res.end()
+        },
+      },
     },
   },
   plugins: [
