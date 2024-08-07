1992a4ec54f930192fc53d84b4e2d77bb4061b48
pengzhanbo <volodymyr@foxmail.com>
2024-08-08 01:09:17 +0800

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,21 @@
-import type { Options } from 'tsup'
+import { type Options, defineConfig } from 'tsup'
 
-export const tsup: Options = {
-  entry: [
-    'src/{index,rsbuild,json5-loader,helper,server}.ts',
-  ],
-  format: ['cjs', 'esm'],
+const shared: Options = {
   dts: true,
   splitting: true,
   clean: true,
   shims: false,
 }
+
+export default defineConfig([{
+  ...shared,
+  entry: [
+    'src/{index,rsbuild,helper,server}.ts',
+  ],
+  format: ['cjs', 'esm'],
+}, {
+  ...shared,
+  entry: ['src/json5-loader.cts'],
+  format: 'cjs',
+  dts: false,
+}])
