50715c8e4767050d8d138947799cbc93f64f2b30
pengzhanbo <volodymyr@foxmail.com>
2026-08-10 17:37:35 +0800

    chore: improve bundle output

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,6 @@
 import type { UserConfig } from 'tsdown'
+import fs from 'node:fs/promises'
+import parse from 'js-tokens'
 import { defineConfig } from 'tsdown'
 
 const config: UserConfig[] = defineConfig([{
@@ -17,6 +19,25 @@ const config: UserConfig[] = defineConfig([{
   sourcemap: false,
   dts: true,
   fixedExtension: false,
+  async onSuccess(config) {
+    for await (const file of fs.glob('*.js', { cwd: config.outDir })) {
+      const filepath = `${config.outDir}/${file}`
+      const code = await fs.readFile(filepath, 'utf-8')
+      await fs.writeFile(filepath, strip(code))
+    }
+  },
 }])
+
+function strip(code: string): string {
+  let result = ''
+  const tokens = parse(code)
+  for (const token of tokens) {
+    if (token.type === 'MultiLineComment' || token.type === 'SingleLineComment') {
+      continue
+    }
+    result += token.value
+  }
+  return result
+}
 
 export default config
